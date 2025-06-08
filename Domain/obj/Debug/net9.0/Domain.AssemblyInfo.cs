3455304c99e0e8eadba3015bc88612431538b9b3
ahmed <ahmed@gmail.com>
2025-06-08 14:28:24 +0300

    removefile

--- Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+788873d542dd90d3f115a94c81a163a01e53f0a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3a2f9a6335ec577427eeafccda2a0acd5ea78a0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
